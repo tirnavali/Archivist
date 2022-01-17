757f68caf97e79c1e091e99666ec96bb72637ebb
Sercan TIRNAVALI <tran.ce.co@gmail.com>
2022-01-17 20:34:42 +0300

    Semantic ui added to project

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,7 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js", preload: true
+pin "semantic-ui", to: "https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js", preload: true
+
 pin_all_from "app/javascript/controllers", under: "controllers"
