7cb11acae116765ed26b6b4de362a5ee36b62ee5
Sercan TIRNAVALI <tran.ce.co@gmail.com>
2022-01-25 16:57:12 +0300

    Validations added to documents model.

--- app/models/document.rb
+++ app/models/document.rb
@@ -10,5 +10,6 @@ class Document < ApplicationRecord
   belongs_to :special_number
   has_and_belongs_to_many :subjects
   validates_comparison_of :ending_date, greater_than: :starting_date, allow_nil: true
+  validates :organization_code, :box, :order, :summary, :starting_date ,presence: true 
   
 end
