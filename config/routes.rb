946c0e41911b8a80a49f9115d409b3baa574de1c
tirnavali <tran.ce.co@gmail.com>
2022-01-26 21:00:30 +0300

    devise added to app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :special_numbers
   resources :special_number_names
   resources :people
@@ -11,5 +12,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root to: "home#index"
+  
 end
