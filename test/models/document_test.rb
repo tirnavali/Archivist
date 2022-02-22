e3d125e515148145e84455916b1b4cb8f281baec
Sercan Tırnavalı <sercan@Sercan-MacBook-Air.local>
2022-02-23 00:04:48 +0300

    Tests updated.

--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class DocumentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save document without summary" do
+    doc = Document.new
+    assert_not doc.save
+  end
 end
