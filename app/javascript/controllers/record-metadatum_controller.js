1bb52965bb361723d00688e1bfeeab1ea5857c2d
Sercan Tırnavalı <tran.ce.co@gmail.com>
2022-03-04 21:29:26 +0300

    select multiple is not working

--- app/javascript/controllers/record-metadatum_controller.js
+++ app/javascript/controllers/record-metadatum_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
   connect() {
     console.log("select2 is here")
 
-    
+    $('.js-example-basic-multiple').select2();
     $('.select2').select2();
   }
 }
