2ef481117e91612588bdf4b9c677b8324d2c3fd5
Sercan Tırnavalı <tran.ce.co@gmail.com>
2022-03-18 18:03:28 +0300

    privacies under work

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -6,6 +6,6 @@ class Audit < ApplicationRecord
   scope :create_actions, -> { where(action: "create")}
   scope :update_actions, -> { where(action: "update")}
   scope :user_activities, ->(user) { where("user_id = ?", user.id).order("created_at desc") }
-  scope :today, ->{ where("created_at" => time_range_today) }
+  scope :today, ->{ where("created_at" => Time.now.midnight..Time.now.tomorrow.midnight ) }
 end
   
