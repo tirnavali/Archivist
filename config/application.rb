5201f4ecc0588a54fef56f5fd2bd8f8dfd080298
Sercan Tırnavalı <tran.ce.co@gmail.com>
2022-03-28 10:47:49 +0300

    Semantic-ui problems solved by moving file content to public folder.

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,6 +12,8 @@ module Archivist
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    
+
 
     # Configuration for the application, engines, and railties goes here.
     #
