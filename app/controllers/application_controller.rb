98143ccbec7c9d27f6558d1624755b8dba2ab763
Sercan Tırnavalı <tran.ce.co@gmail.com>
2022-04-08 17:53:01 +0300

    Step added to record_metadata form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:role])
   end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,17 @@
 module ApplicationHelper
-    def is_page_active?(current_page_path)
-        if current_page?(current_page_path)
-            "active teal item"
-        else 
-            "teal item"
-        end
+  def is_page_active?(current_page_path)
+    if current_page?(current_page_path)
+      "active teal item"
+    else 
+      "teal item"
     end
+  end
 
-    def user_superadmin?
-      current_user.superadmin?
-    end
+  def user_superadmin?
+    current_user.superadmin?
+  end
 
-
+  def record_navigation (attachment_id=0)
+    render "layouts/shared/record_nav", record_attachment_id: attachment_id
+  end
 end
