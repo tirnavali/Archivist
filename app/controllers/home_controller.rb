da94940501741f8816ae50749e2d7e74b07e9c2c
Sercan Tırnavalı <tran.ce.co@gmail.com>
2022-06-01 18:00:35 +0300

    Home controller updated! Needs to fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,16 @@
 class HomeController < ApplicationController
   def index
     @q = RecordMetadatum.ransack(params[:q])
-    @record_metadata = @q.result.includes(:fond, :subjects, :toponyms)
-    @record_metadata = RecordMetadatum.order(:created_at).limit 5
+    
+    if (params[:q]).nil?
+      @q = RecordMetadatum.ransack(params[:q])
+      @record_metadata = @q.result.page.limit 2 # burada sorun var :D
+    else
+      
+      @record_metadata = @q.result.page params[:page]
+    end
+      # @record_metadata = (@q.result.includes(:fond, :subjects, :toponyms).page, params[:page])
   end
 
+ 
 end
