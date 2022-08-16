038ea632e93adf7a671dca77134099ec94311d75
Sercan Tırnavalı <tran.ce.co@gmail.com>
2022-08-16 15:00:02 +0300

    fond path view added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def is_page_active?(current_page_path)
     if current_page?(current_page_path)
-      "active teal item"
+      "active  item"
     else 
-      "teal item"
+      " item"
     end
   end
 
--- app/helpers/fonds_helper.rb
+++ app/helpers/fonds_helper.rb
@@ -1,2 +1,13 @@
 module FondsHelper
+  # Returns hierarcial objects of parents
+  def pparent(fond, arr = [])
+      tem_arr = arr
+    if fond.parent
+      tem_arr << fond
+      pparent(fond.parent, tem_arr)
+    elsif fond.parent.nil? && fond.depth == 0
+      tem_arr << fond
+    end
+    return tem_arr.reverse
+  end
 end
