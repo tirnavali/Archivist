b59c298f96b0c739284e027973044f728791b7af
Sercan Tırnavalı <tran.ce.co@gmail.com>
2022-09-14 17:45:57 +0300

    Home facet helper added to app.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,15 +2,16 @@ module HomeHelper
   #takes Sunspot::Search::FieldFacet 
   def facet_helper(field_facet, result_size=10, field_facet_params)
     inputs = ""
-    
-    field_facet.rows[1..result_size.to_i].each do |facet|
-      input = ERB.new <<-TEMP
-        <li>
-          <%= check_box_tag "#{field_facet.name}[#{facet.value}]", "#{facet.value}", #{(!field_facet_params.nil?) && (field_facet_params.include?(facet.value))} %>
-          <%= label_tag "#{facet.value} #{facet.count}" %>
-        </li>
-        TEMP
-    inputs << input.result(binding).html_safe
+    unless field_facet.nil? || field_facet.rows.size == 0
+      field_facet.rows[1..result_size.to_i].each do |facet|
+        input = ERB.new <<-TEMP
+          <li>
+            <%= check_box_tag "#{field_facet.name}[#{facet.value}]", "#{facet.value}", #{(!field_facet_params.nil?) && (field_facet_params.include?(facet.value))} %>
+            <%= label_tag "#{facet.value} #{facet.count}" %>
+          </li>
+          TEMP
+      inputs << input.result(binding).html_safe
+      end
     end
     
     template = ERB.new <<-TEMPLATE
@@ -21,21 +22,5 @@ module HomeHelper
         template.result(binding).html_safe
       end
   end
-
-  def facet_helper2(field_facet)
-    template = ERB.new <<-TEMPLATE
-    <% unless field_facet.nil? || field_facet.rows.size == 0 %>
-        <div class="row">
-          <h3><%= t(:toponyms) %></h3>
-          <% field_facet.rows[1..10].each do |facet| %>
-            <li>
-              <%= check_box_tag "toponyms[]", facet.value, ((!params[:toponyms].nil?) && (params[:toponyms].include? facet.value)) %>
-              <%= label_tag "" %>
-            </li>
-          <% end %> 
-        </div>
-      <% end %>
-      TEMPLATE
-    template.result(binding).html_safe
-  end
+  
 end
